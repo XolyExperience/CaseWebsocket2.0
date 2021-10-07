2368480d0dd0bf0d5c3ce90ecacbcfed6ea3c314
Beff <Beff.Henriksson@live.se>
2021-10-07 15:27:31 +0200

    Added comments to previous committed
    Added reminder to remove CORS before deploying code

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const cors = require("cors");
+const cors = require("cors"); // Remove after devolpment, not used in deployed code
 import { express } from "express";
 const app = express();
 
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 const buildPath = path.join(__dirname, "..", "build");
 app.use(express.static(buildPath));
-app.use(cors());
+app.use(cors()); // Remove after devolpment, not used in deployed code
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
