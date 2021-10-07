f90e800d90968bd127cea1f4f79425051d0ac953
Beff <Beff.Henriksson@live.se>
2021-10-07 15:21:49 +0200

    Installed cors and set buildpath
    Added CORS and set buildpath for heroku

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 require("dotenv").config();
+const cors = require("cors");
 import { express } from "express";
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+const buildPath = path.join(__dirname, "..", "build");
+app.use(express.static(buildPath));
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
